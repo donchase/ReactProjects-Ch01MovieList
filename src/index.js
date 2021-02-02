8d784c8db4356ec40851e3840734f146a4933f8e
Don Chase <donchase@gmail.com>
2021-02-02 10:53:36 -0500

    Setting up

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// console.log("movieList v4");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+    return <h1>Movie List Page v4</h1>;
+};
+ReactDOM.render(<App />, document.getElementById('root'));
